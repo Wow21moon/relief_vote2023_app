aa8e35b21c629292f4bbc01fd0aac1d0c46e4fba
Denis Merinov <wow21moon@yandex.ru>
2022-12-07 10:52:58 +0300

    two3

--- src/components/WorkerItem/WorkerItem.jsx
+++ src/components/WorkerItem/WorkerItem.jsx
@@ -1,14 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './WorkerItem.css'
 
-export const WorkerItem = ({worker}) => {
+export const WorkerItem = ({ worker, changeVote, activeVotes }) => {
     
 
+    useEffect(() => {
+        const activeClass = activeVotes.find(item => item.id === worker.id)
+
+        console.log('activeClass', activeClass)
+
+        if(activeClass) {
+            setClassActive('active')
+        } else {
+            setClassActive('')
+        }
+
+    }, [activeVotes, worker])
+
+    const [classActive, setClassActive] = useState(true)
+
+    const onAddHandler = () => {
+        changeVote(worker, setClassActive)
+    }
+
+
+
     return (
-        <div className='worker'>
-            <img src={worker.photo_link} alt="" />
-            <div>{worker.name}</div>
-            <div>{worker.id}</div>
+        <div className={'worker ' + classActive} onClick={onAddHandler}>
+            <img
+                src="images/check.svg"
+                alt="check"
+                className={'worker__check'}
+            />
+            <img
+                src={worker.photo_link}
+                alt={worker.name}
+                className={'worker__photo'}
+            />
+            <div className="worker__name">{worker.name}</div>
         </div>
     )
 }
--- src/components/WorkerList/WorkerList.jsx
+++ src/components/WorkerList/WorkerList.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { WorkerItem } from '../WorkerItem/WorkerItem'
 import './WorkerList.css'
 
 export const WorkerList = () => {
+
+    const [activeVotes, setActiveVotes] = useState([])
+
+
+    const changeVote = (worker, setClass) => {
+
+        const alreadyVote = activeVotes.find(item => item.id === worker.id)
+
+        let newItems = []
+
+        if(alreadyVote) {
+            newItems = activeVotes.filter(item => item.id !== worker.id)
+            
+        } else {
+            if (activeVotes.length > 2) {
+                setClass('disabled')
+                setTimeout(function(){
+                    setClass('')
+                }, 1000)
+                return
+            }
+
+            newItems = [...activeVotes, worker]
+        }
+
+        setActiveVotes(newItems)
+
+    }
+
+    console.log(activeVotes)
+
     const data = [
         { name: 'Иван Сергеев', photo_link: 'images/ivan.jpg', id: '1' },
         { name: 'Алексей Иванов', photo_link: 'images/alexei.jpg', id: '2' },
@@ -12,11 +43,13 @@ export const WorkerList = () => {
         { name: 'Наталья Артемова', photo_link: 'images/natalie.jpg', id: '6' },
     ]
 
+
+
     return (
         <div className="worker-list__wrapper">
             <ul className="worker-list__container">
                 {data.map((item) => (
-                    <WorkerItem worker={item}/>
+                    <WorkerItem worker={item} changeVote={changeVote} key={item.id} activeVotes={activeVotes}/>
                 ))}
             </ul>
         </div>
